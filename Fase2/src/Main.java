51510ebd3ad67a976639fd148af51d161ea98210
Michelle Mejía Villela <97720056+michellemej22596@users.noreply.github.com>
2023-03-04 20:15:43 -0600

    Numeros y lector

--- Fase2/src/Main.java
+++ Fase2/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Interpretador interpretador = new Interpretador();
+        interpretador.reader();
     }
 }
