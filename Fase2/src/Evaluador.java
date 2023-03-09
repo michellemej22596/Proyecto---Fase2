d3f1edeec122d4415f871bef0fac3582fc74b046
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-03-09 11:25:53 -0600

    Aqui andamos probando que peo

--- Fase2/src/Evaluador.java
+++ Fase2/src/Evaluador.java
@@ -1,4 +1,48 @@
+
+import java.util.Stack;
 public class Evaluador {
+
+    public Double operador(String[] valores){
+        for (int i=0; i<valores.length; i++) {
+            switch (valores[i]) {
+                case "(": 
+                    Stack<Double> stack = new Stack<Double>();
+
+
+                    break;
+            }
+
+    }
+
+    return null;
+
+       Double a = mystack.pop();
+       Double b = mystack.pop();
+
+       return a;
+
+
+
+
+
+
+    }
+
+    public Stack<Double> stackeador(String[] valores){
+
+        for (int i=0; i<valores.length; i++) {
+                switch (valores[i]) {
+                    case "(": 
+                        Stack<Double> stack = new Stack<Double>();
+
+
+                        break;
+                }
+
+        }
+
+        return null;
+    }
 	
 	
 	
--- Fase2/src/Interpretador.java
+++ Fase2/src/Interpretador.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Stack;
 
 public class Interpretador {
 	
