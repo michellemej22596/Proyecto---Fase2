7b6ccc029708c7e685e3f77745fa13cacfbff4d3
Michelle Mejía Villela <97720056+michellemej22596@users.noreply.github.com>
2023-03-14 23:25:49 -0600

    Firma métodos de clase Estructura

--- Fase2/src/Estructura.java
+++ Fase2/src/Estructura.java
@@ -1,3 +1,32 @@
-public class Estructura {
+import java.util.ArrayList;
 
+public class Estructura {
+	
+    public String operar(ArrayList<String> linea) {
+    	
+       return null;
+    }
+    
+    private ArrayList<String> cond(ArrayList<String> linea) {
+       
+        return null;
+    }
+    
+    private boolean condResult(ArrayList<String> condFragment) {
+        return false;
+    }
+
+
+    private String quote(ArrayList<String> linea) {
+        
+    	return null;
+    }
+
+    private String accessListElement(ArrayList<String> linea) {
+        return null;
+    }
+
+    public ArrayList<String> list(ArrayList<String> linea) {
+        return null;
+    }
 }
