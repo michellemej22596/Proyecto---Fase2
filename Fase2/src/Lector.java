9ee76f03ad4ba3b1d9b73ce02bdca9333409a811
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-03-14 23:39:16 -0600

    No se usar regex entonces me tocó fuarmela, me tomó menos tiempo sacarlo así que intentar entender regex xd

--- Fase2/src/Lector.java
+++ Fase2/src/Lector.java
@@ -6,13 +6,14 @@ public class Lector implements  IStack{
 // Para hacer la lectura del txt nos guíamos de la siguiente fuente: https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
     public ArrayList<ArrayList<String>> getStack(String document) {
         System.out.println("a");
+        ArrayList<ArrayList<String>> stack = new ArrayList<>();
         try {
         BufferedReader reader = new BufferedReader(new FileReader(document));
         String linea ="";
         int inicio = 0;
         int fin = 0;
         String substring = "";
-        ArrayList<String> lineToCalculate = new ArrayList<>();
+        ArrayList<String> arr = new ArrayList<>();
         
 
             while((linea=reader.readLine()) != null){
@@ -40,13 +41,71 @@ public class Lector implements  IStack{
                             case '/':
                             if(actual == '(') {
                                 inicio ++;
-                            } else if(actual == ')') {
+                            } 
+                            else if(actual == ')') {
                                 fin ++;
-                           
+                            }
+                            if(actual == '<' || actual == '>' || actual == '=') {
+                                if(siguiente == '=') {
+                                    System.out.println("INNN");
+                                    substring += actual;
+                                    break;
+                                }
+                            } 
+                            else if(actual == '-') {
+                                if(siguiente != ' ') {
+                                    substring += actual;
+                                    break;
+                                }
+                            }
+                            if(actual == '=') {
+                                if(siguiente =='=') {
+                                    substring += actual;
+                                }
+                            }  
+                            
+                            if(substring.length() > 0 && substring.charAt(0) != '"' && actual != '"') {
+                                arr.add(substring);
+                                substring = "";
+                            }
 
+                            if(actual != ' ' && actual != '"' && substring.length() == 0){
+                                substring += actual;
+                                arr.add(substring);
+                                substring = "";
+                            }
 
-                        }
+                            if(actual == '"'){
+                                if(substring.length() > 0) {
+                                    substring += actual;
+                                    arr.add(substring);
+                                    substring = "";
+                                } else {
+                                    substring += actual;
+                                }
+                            }
 
+                            if(substring.length() > 0 && substring.charAt(0) == '"') {
+                                if(substring.length() > 1) {
+                                    substring+= actual;
+                                }
+                            }
+                            break;
+
+                            default:    // Gets any character that doesn't define an operation
+                            substring += actual;
+                            if(i == linea.length() - 1) {
+                                arr.add(substring);
+                            }
+                            break;
+                    }
+
+                    if(inicio == fin && arr.size() > 0) {
+                        ArrayList<String> arr2 = new ArrayList<>(arr);
+                        stack.add(arr2);
+                        arr.clear();
+                        inicio = 0;
+                        fin = 0;
                     }
 
                 }
@@ -54,13 +113,14 @@ public class Lector implements  IStack{
             }
 
         }
+    }
 
         catch (Exception e){
             System.err.println("No se pudo cargar el Stack");
         }
        
-        return null;
-    }
+        return stack;
+    
 
 
 
@@ -69,4 +129,5 @@ public class Lector implements  IStack{
 
 
 
+}
 }
