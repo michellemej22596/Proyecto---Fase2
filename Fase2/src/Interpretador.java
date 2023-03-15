b81759b2dd85834834da3912646a98f2f8ae63f0
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-03-14 21:37:11 -0600

    voy a separar la función y le voy a dar su propia clase, para no tener un montón de funciones en el interpretador y separar clases según su función

--- Fase2/src/Interpretador.java
+++ Fase2/src/Interpretador.java
@@ -7,58 +7,6 @@ import java.util.LinkedList;
 
 public class Interpretador {
 		
-	public void reader() {
-		//The following code to read a text file was found at https://chuwiki.chuidiang.org/index.php?title=Lectura_y_Escritura_de_Ficheros_en_Java#:~:text=Podemos%20abrir%20un%20fichero%20de,y%20extraer%20campos%20de%20ella.
-		//Some lines have been edited in order to read properly the text file requested and split the characters.
-		File archivo = null;
-		FileReader fr = null;
-		BufferedReader br = null;
-		String msg="";
-		
-		try {
-			
-		    archivo = new File ("C:\\lisp.txt");
-		    fr = new FileReader (archivo);
-		    br = new BufferedReader(fr);
-		    
-		    System.out.println("Su archivo ha sido leído con éxito.");
-		    System.out.println("");
-		    System.out.println("Contenido encontrado: ");
-		
-		    // Lectura 
-		    String linea;
-		    while((linea=br.readLine())!=null) {
-		       System.out.println(linea);
-		    	msg=linea;
-		    	String[] parts = msg.split(" ");
-		    	
-		    }
-		    
-		  
-		}
-		
-		catch(Exception e){
-			e.printStackTrace();
-		    System.out.println("No se ha encontrado el archivo de texto");
-		    System.out.println("Asegúrese de llamarlo lisp.txt en su disco C");
-		 }
-		
-		finally{
-			
-		    try{   
-		    	
-		       if( null != fr ){   
-		          fr.close();     
-		       }   
-		    }
-		    
-		    catch (Exception e2){ 
-		       e2.printStackTrace();
-		    }
-		
-		}
-				
-	}
 
 
 
